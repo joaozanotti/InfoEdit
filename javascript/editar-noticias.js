54f2af37792a07443ed8772715e78e6338e53e51
Sindy-Catalunha <sindycatalunha@gmail.com>
2023-07-20 18:42:44 -0300

    Atualizando comentários

--- javascript/editar-noticias.js
+++ javascript/editar-noticias.js
@@ -4,54 +4,64 @@ const outMaterias = document.getElementById("outMaterias");
 const btEnviar = document.getElementById("btEnviar");
 const formEditar = document.getElementById("formEditar");
 formEditar.style.display = "none";
-
+//Adicionando evento no botão de pesquisa de notícias na página de edição;
 btPesquisaEditar.addEventListener("click", pesquisaEditar);
 
 function pesquisaEditar() {
-  var pesquisaTitulo = (inPesquisaEditar.value).toUpperCase();
-  formEditar.style.display = "none";
-  outMaterias.innerHTML = "";
+  var pesquisaTitulo = (inPesquisaEditar.value).toUpperCase(); //Transformando toda a entrada do input em maiúsculo;
+  formEditar.style.display = "none"; //Invisibilizando a formatação do layout;
+  outMaterias.innerHTML = ""; //Saída de informações;
 
-    if (inPesquisaEditar.value == "") {
+    //Verificação de entradas;
+    if (inPesquisaEditar.value == "") { 
         alert("Digite o título.");
         inPesquisaEditar.focus();
         outMaterias.innerHTML = "";
 
     } else {
         for (var i = 0; i < vetTitulo.length; i++) {
-            let autorAux = vetTitulo[i].toUpperCase();
-            if (autorAux.indexOf(pesquisaTitulo) >= 0) {
-                var divMateria = document.createElement("div");
+            let autorAux = vetTitulo[i].toUpperCase(); //Transformando a entrada de palavras em maiúsculo;
+            if (autorAux.indexOf(pesquisaTitulo) >= 0) { //Identificação da parte escrita do usuário com o vetor;
+                var divMateria = document.createElement("div"); //Caso encontre, é criado elementos no html para alojar o conteúdo;
                 divMateria.className = "materia";
     
-                var imgHtml = document.createElement("img")
+                var imgHtml = document.createElement("img"); // Inserindo imagem;
                 imgHtml.src = "../imagens/ImagensNoticias/" + vetImagens[i];
                 imgHtml.className = "materia-imagem";
     
-                var h3 = document.createElement("h3");
-                h3.innerHTML = vetTitulo[i];
-                h3.id = "noticia" + i;
+                var h3 = document.createElement("h3"); //Tag para o título;
+
+                h3.innerHTML = vetTitulo[i]; //Colocando o título na tag criada para ele;
+                h3.id = "noticia" + i; // adiciona i para saber em que conteúdo do vetor irá mostrar;
+
+                //Adicionando evento ao clicar no título;
                 h3.addEventListener("click", cliqueTitulo);
     
-                var h6 = document.createElement("h6");
-                h6.innerHTML = vetAutor[i];
+                var h6 = document.createElement("h6"); //Tag para autor;
+
+                h6.innerHTML = vetAutor[i]; //Colocando o título na tag criada para ele;
                 h6.className = "autor";
     
-                var button = document.createElement("input");
+                var button = document.createElement("input"); //Botão de publicar a edição;
+
                 button.type = "button";
                 button.value = "Editar";
-                button.id = "btEditar" + i;
-                button.addEventListener("click", inserirDados);
+                button.id = "btEditar" + i; // adiciona i para saber em que conteúdo do vetor irá mostrar;
+
+                button.addEventListener("click", inserirDados); // Adicionando evento no botão (clicar);
     
+                // Colocando partes de uma notícia na mesma caixa;
                 divMateria.appendChild(imgHtml);
                 divMateria.appendChild(h3);
                 divMateria.appendChild(h6);
                 divMateria.appendChild(button);
     
+                // Expor notícia (img+título+autor);
                 outMaterias.appendChild(divMateria);
             }
         }
-        if (outMaterias.innerHTML == "") {
+        if (outMaterias.innerHTML == "") { //Caso a notícia/parte da notícia procurada não está no vetor;
+
             outMaterias.innerHTML = "Nenhuma matéria encontrada.";
         }
     }
@@ -60,10 +70,12 @@ function pesquisaEditar() {
 var idAtual;
 
 function inserirDados() {
+    //Função para editar notícia clicada;
     var idClicado = this.id;
-    idAtual = idClicado.substring(8);
+    idAtual = idClicado.substring(8); //Vetor do botão em: "btEditar" + i;
 
     formEditar.style.display = "block";
+    //Colocar o conteúdo de uma notícia nos campos de editar - abre campo de edição;
     inTitulo.value = vetTitulo[idAtual];
     inTexto.value = vetTexto[idAtual];
     inAutor.value = vetAutor[idAtual];
@@ -72,6 +84,7 @@ function inserirDados() {
 btEnviar.addEventListener("click", editar);
 
 function editar() {
+    //Verificação se foi alterado algo ou não no campo de edição;
     if (vetTitulo[idAtual] == inTitulo.value && vetTexto[idAtual] == inTexto.value && vetAutor[idAtual] == inAutor.value) {
         alert("Realize alguma edição.");
 
@@ -79,6 +92,7 @@ function editar() {
         alert("Os dados não podem ser vazios.");
 
     } else {
+        //Após as verificações, concluir a alteração do usuário;
         vetTitulo[idAtual] = inTitulo.value;
         vetTexto[idAtual] = inTexto.value;
         vetAutor[idAtual] = inAutor.value;
@@ -88,41 +102,43 @@ function editar() {
     }
 }
 
-function cliqueTitulo() {
-    outMaterias.innerHTML = "";
-    formEditar.style.display = "none";
+function cliqueTitulo() { //Botão que entra no conteúdo inteiro da notícia no clique do título;
+    outMaterias.innerHTML = ""; //Saída de informações;
+    formEditar.style.display = "none"; //Tirando a formatação da visibilidade do usuário;
 
-    var idClicado = this.id;
-    var idAtual = idClicado.substring(7);
+    var idClicado = this.id; //Pegar todo conteúdo do ID do click;
+    var idAtual = idClicado.substring(7); //Pega um símbolo selecionado pela posição do nome do id criado para o elemento clicado (título);
 
-    var divMateria = document.createElement("div");
+    var divMateria = document.createElement("div"); //Caso encontre, é criado elementos no html para alojar o conteúdo;
     divMateria.className = "materia";
 
-    var h3 = document.createElement("h3");
+    var h3 = document.createElement("h3"); //Tag para o título;
     h3.innerHTML = vetTitulo[idAtual];
 
-    var img = document.createElement("img");
-    img.src = "../imagens/ImagensNoticias/" + vetImagens[idAtual];
+    var img = document.createElement("img"); // Inserindo imagem;
+    img.src = "../imagens/ImagensNoticias/" + vetImagens[idAtual]; //Mostrar imagem - idAtual é o número da posição dado no nome do id do elemento;
     img.className = "materia-imagem";
 
     var texto = document.createElement("article");
-    texto.innerHTML = vetTexto[idAtual];
+    texto.innerHTML = vetTexto[idAtual]; //Mostrar texto da notícia;
 
-    var h6 = document.createElement("h6");
-    h6.innerHTML = vetAutor[idAtual];
+    var h6 = document.createElement("h6"); // Inserindo autor;
+    h6.innerHTML = vetAutor[idAtual]; //Mostrar autor da notícia;
     h6.className = "autor";
 
-    var button = document.createElement("input");
+    var button = document.createElement("input"); //Botão de publicar a edição;
     button.type = "button";
     button.value = "Editar";
-    button.id = "btEditar" + idAtual;
-    button.addEventListener("click", inserirDados);
+    button.id = "btEditar" + idAtual; // adiciona idAtual para saber em que conteúdo do vetor irá mostrar;
+    button.addEventListener("click", inserirDados); // Adicionando evento no botão (clicar);
 
+    // Colocando partes de uma notícia na mesma caixa;
     divMateria.appendChild(h3);
     divMateria.appendChild(img);
     divMateria.appendChild(texto);
     divMateria.appendChild(h6);
     divMateria.appendChild(button);
 
+    // Expor notícia (img+título+autor);
     outMaterias.appendChild(divMateria);
 }

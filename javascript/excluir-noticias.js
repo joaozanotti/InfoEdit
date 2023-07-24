8d2bb5b23db0df2f668f50e4f1b9f5a7ee5c9432
joaozanotti <joaovzanotti@gmail.com>
2023-07-24 16:01:09 -0300

    Comentando os javascripts

--- javascript/excluir-noticias.js
+++ javascript/excluir-noticias.js
@@ -1,72 +1,83 @@
+// Referenciando os elementos no documento HTML
 const inPesquisaExcluir = document.getElementById("inPesquisaExcluir");
 const btPesquisaExcluir = document.getElementById("btPesquisaExcluir");
 const outMaterias = document.getElementById("outMaterias");
 const btExcluir = document.getElementById("btExcluir");
 
-//CRIANDO BOTAO PARA ACESSO DA FUNCION DE PESQUISAR;
+// Adicionando evento de clique no botão para acionamento da function de pesquisa
 btPesquisaExcluir.addEventListener("click", pesquisarExcluir);
 
+// Criando a function de pesquisa por título
 function pesquisarExcluir() {
+    // Recebendo o texto que o usuário digitou e transformando em maiúsculo
+    var pesquisaTitulo = (inPesquisaExcluir.value).toUpperCase();
 
-    var pesquisaTitulo = (inPesquisaExcluir.value).toUpperCase();// TRANSFORMANDO TUDO EM MAIÚSCULO
-
+    // Limpando o parágrafo de saída em caso de nova pesquisa
     outMaterias.innerHTML = "";
 
+    // Verificando se o usuário digitou algo ou não
     if (inPesquisaExcluir.value == "") {
-
         alert("Digite um título.");
         inPesquisaExcluir.focus();
         outMaterias.innerHTML = "";
-
-
-        //CRIANDO UMA ESTRUTURA DE REPETIÇÁO PARA PESQUISAR 
+ 
     } else {
+        // Criando a estrutura de repetição para percorrer o vetor de título
         for (var i = 0; i < vetTitulo.length; i++) {
 
+            // Transformando o conteúdo da posição específica do vetor em maiúsculo para se encaixar com a pesquisa
             let autorAux = vetTitulo[i].toUpperCase();
 
+            //Verificação de pertencimento das letras preenchidas pelo usuário em algum determinado elemento de um vetor
             if (autorAux.indexOf(pesquisaTitulo) >= 0) {
+
+                // Criando os elementos com seus determinados atributos para serem exibidos na página
+
+                // Criando divs para estilização
                 var divContainerMateria = document.createElement("div");
                 divContainerMateria.className = "materia-container";
 
-                //EXIBINDO AS IMAGENS DAS NOTICIAS;
+                // Criando as imagens das notícias
                 var img = document.createElement("img");
                 img.src = "../imagens/ImagensNoticias/" + vetImagens[i];
                 img.className = "materia-imagem";
 
-                //CRIANDO DIVS PARA ESTILIZAÇÃO
+                // Criando divs para estilização
                 var divConteudoMateria = document.createElement("div");
                 divConteudoMateria.className = "materia-conteudo";
 
-                //MOSTRANDO OS TITULOS DA MATERIA E DANDO UM ID PARA ELES;
+                // Criando o título da matéria
                 var titulo = document.createElement("h1");
                 titulo.className = "materia-titulo";
                 titulo.innerHTML = vetTitulo[i];
+                // Adicionando um id no título para depois usarmos na função de clique no título
                 titulo.id = "noticia" + i;
                 titulo.addEventListener("click", cliqueTituloExcluir);
 
-                //MOSTRANDO NOME DO AUTOR;
+                // Criando nome do autor
                 var autor = document.createElement("h3");
                 autor.innerHTML = vetAutor[i];
                 autor.className = "materia-autor";
 
-                //CRIANDO BOTÃO DE EXCLUIR COM ID; 
+                // Criando botão de excluir
                 var button = document.createElement("input");
                 button.type = "button";
                 button.value = "Excluir";
+                // Adicionando um id no botão para depois usarmos na função de excluir dados
                 button.id = "btExcluir" + i;
                 button.className = "materia-button";
                 button.addEventListener("click", excluirDados);
 
-                //JUNTANDO IMG/TITULO/AUTOR/BUTTON EM "divConteudoMateria" PARA EXIBIR AS NOTICIAS;
+                // Colocando img em uma div e o titulo/autor/button em outra div
                 divContainerMateria.appendChild(img);
                 divConteudoMateria.appendChild(titulo);
                 divConteudoMateria.appendChild(autor);
                 divConteudoMateria.appendChild(button);
 
+                // Juntando as duas divs para formatação
                 divContainerMateria.appendChild(divConteudoMateria);
 
-                //EXIBINDO AS NOTICIAS;
+                // Exibindo as notícias
                 outMaterias.appendChild(divContainerMateria);
             }
             // Verificando se nenhuma notícia foi encontrada e exibindo uma mensagem de alerta
@@ -75,65 +86,79 @@ function pesquisarExcluir() {
             }
         }
     }
-    //FUNCTION INCIADA AO CLICK NO TITULO DA MATERIA 
-    function cliqueTituloExcluir() {
-        outMaterias.innerHTML = "";
-
-        var idClicado = this.id;
-        var idAtual = idClicado.substring(7);
-
-        //ESTILIZAÇÃO DO TEXTO
-        var artMaterias = document.createElement("article");
-        artMaterias.className = "noticia-container";
-
-        //MOSTRANDO OS TITULOS DA MATERIA;
-        var titulo = document.createElement("h1");
-        titulo.innerHTML = vetTitulo[idAtual];
-        titulo.className = "noticia-titulo";
-
-        //EXIBINDO AS IMAGENS DAS NOTICIAS;
-        var img = document.createElement("img");
-        img.src = "../imagens/ImagensNoticias/" + vetImagens[idAtual];
-        img.className = "noticia-imagem";
-
-        //EXIBINDO O TEXTO DAS NOTICIAS;
-        var texto = document.createElement("p");
-        texto.innerHTML = vetTexto[idAtual];
-        texto.className = "noticia-texto";
-
-        //MOSTRANDO NOME DO AUTOR;
-        var autor = document.createElement("h3");
-        autor.innerHTML = vetAutor[idAtual];
-        autor.className = "noticia-autor";
-
-        //CRIANDO O BOTAO EXCLUIR DENTRO DO CLICK TITULO;
-        var button = document.createElement("input");
-        button.type = "button";
-        button.value = "Excluir";
-        button.id = "btExcluir" + idAtual;
-        button.className = "noticia-button";
-        button.addEventListener("click", excluirDados);
-
-        //JUNTANDO IMG/TITULO/TEXTO/AUTOR/BUTTON EM "divConteudoMateria" PARA EXIBIR AS NOTICIAS;
-        artMaterias.appendChild(titulo);
-        artMaterias.appendChild(img);
-        artMaterias.appendChild(texto);
-        artMaterias.appendChild(autor);
-        artMaterias.appendChild(button);
-
-        outMaterias.appendChild(artMaterias);
-    }
 }
-function excluirDados() {
-    var idClicado = this.id; //VERIFICA O ID DO BOTAO,E GUARDA O ID DO BOTAO CLICADO "btExcluir(0 a 5)"
-    idAtual = idClicado.substring(9);  //PEGA O CARACTERE DE NUMERO 9 E GUARDA; 
 
+// Criando a function de clicar no título e entrar na notícia
+function cliqueTituloExcluir() {
+    // Limpando o campode saída
+    outMaterias.innerHTML = "";
+
+    // Pegando o elemento inteiro do título que foi clicado e selecionando apenas o id
+    var idClicado = this.id;
+    // Pegando o sétimo caractere da string do id do título, que será o número
+    var idAtual = idClicado.substring(7);
+
+    // Criando os elementos e exibindo a notícia na página usando o id do botão que foi pego acima
+
+    // Criando o article para estilização do texto
+    var artMaterias = document.createElement("article");
+    artMaterias.className = "noticia-container";
+
+    // Criando o título da matéria
+    var titulo = document.createElement("h1");
+    titulo.innerHTML = vetTitulo[idAtual];
+    titulo.className = "noticia-titulo";
+
+    // Criando a imagem da notícia
+    var img = document.createElement("img");
+    img.src = "../imagens/ImagensNoticias/" + vetImagens[idAtual];
+    img.className = "noticia-imagem";
+
+    // Criando o texto da notícia
+    var texto = document.createElement("p");
+    texto.innerHTML = vetTexto[idAtual];
+    texto.className = "noticia-texto";
+
+    // Criando o nome do autor
+    var autor = document.createElement("h3");
+    autor.innerHTML = vetAutor[idAtual];
+    autor.className = "noticia-autor";
+
+    // Criando o botão de excluir
+    var button = document.createElement("input");
+    button.type = "button";
+    button.value = "Excluir";
+    // Adicionando um id no botão para depois usarmos na função de excluir dados
+    button.id = "btExcluir" + idAtual;
+    button.className = "noticia-button";
+    button.addEventListener("click", excluirDados);
+
+    // Juntando todos os elementos no artigo
+    artMaterias.appendChild(titulo);
+    artMaterias.appendChild(img);
+    artMaterias.appendChild(texto);
+    artMaterias.appendChild(autor);
+    artMaterias.appendChild(button);
+
+    // Exibindo o artigo na página
+    outMaterias.appendChild(artMaterias);
+}
+
+function excluirDados() {
+    // Pegando o elemento inteiro do botão que foi clicado e selecionando apenas o id
+    var idClicado = this.id;
+    // Pegando o nono caractere da string do id do botão, que será o número
+    idAtual = idClicado.substring(9);
+
+    // Seleciona o elemento dentro do vetor usando o idAtual e o próximo número seleciona quantos elementos a serem excluídos;
     vetTitulo.splice(idAtual, 1);
-    vetAutor.splice(idAtual, 1);        //SELECIONA O ELEMENTO DENTRO DO VETOR DA POSIÇÃO idAtual E O PROXIMO NUMERO EXCLUI ELE (1);
+    vetAutor.splice(idAtual, 1);        
     vetImagens.splice(idAtual, 1);
     vetTexto.splice(idAtual, 1);
 
-    inPesquisaExcluir.value = "";    // LIMPAS AS NOTICIAS; 
-    outMaterias.innerHTML = "Notícia Excluida com sucesso!";// DEPOIS DA NOTICIA EXCLUIDA MENSAGEM; 
+    // Limpa o input de pesquisa das notícias
+    inPesquisaExcluir.value = "";
+    // Exibe uma mensagem de confirmação ao usuário
+    outMaterias.innerHTML = "Notícia Excluida com sucesso!";
 
 }
